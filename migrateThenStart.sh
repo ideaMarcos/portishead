9baee3e54e4ef257f183c598adecb8c262abd458
Marcøs <571086+ideaMarcos@users.noreply.github.com>
2019-12-27 09:43:41 -0500

    fix migration

--- migrateThenStart.sh
+++ migrateThenStart.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# Adapted from Alex Kleissner's post, Running a Phoenix 1.3 project with docker-compose
-# https://medium.com/@hex337/running-a-phoenix-1-3-project-with-docker-compose-d82ab55e43cf
 
 set -e
 
 ./bin/portishead eval "Portishead.Release.migrate"
-
 ./bin/portishead start
