d5704a1b93f2bb337f1d874e87aba9fb79ab072f
Marcøs <571086+ideaMarcos@users.noreply.github.com>
2020-01-15 11:06:43 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Portishead.MixProject do
     [
       app: :portishead,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: ">= 1.9.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
