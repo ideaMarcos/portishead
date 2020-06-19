967403920e29c13efc758bb3cb3fb81301eafb49
Marcøs <571086+ideaMarcos@users.noreply.github.com>
2020-06-19 15:49:44 -0400

    country table

---
+++ priv/repo/migrations/20200113205300_create_country_table.exs
@@ -0,0 +1,31 @@
+defmodule Portishead.Repo.Migrations.CreateCountryTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:country, primary_key: false) do
+      add :code, :string, primary_key: true, size: 3
+      add :name, :string, null: false
+    end
+
+    flush()
+
+    directory = Application.app_dir(:portishead, "priv/repo")
+    file_path = Path.join(directory, "country-codes.csv")
+    import_people_from(file_path)
+  end
+
+  defp import_people_from(filepath) do
+    sql = """
+      COPY premarcos.country (code, name)
+      FROM STDIN
+      WITH DELIMITER ',' CSV HEADER
+    """
+
+    stream = Ecto.Adapters.SQL.stream(Portishead.Repo, sql)
+
+    Portishead.Repo.transaction(fn ->
+      File.stream!(filepath)
+      |> Enum.into(stream)
+    end)
+  end
+end
--- priv/repo/migrations/20200113214658_create_band_table.exs
+++ priv/repo/migrations/20200113214658_create_band_table.exs
@@ -4,7 +4,7 @@ defmodule Portishead.Repo.Migrations.CreateBandTable do
   def change do
     create table(:band) do
       add :name, :string, null: false
-      add :country_code, :string
+      add :country_code, references(:country, column: :code, type: :string), null: false, size: 3
 
       add :metadata, :json
       timestamps()
